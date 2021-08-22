e79c6ef16340f45f96ca9105f6bc7fd4d2e690ca
kuma5781 <kumamoto8110@icloud.com>
2021-08-22 15:46:19 +0900

    docker導入

--- project/FrontendCommands.scala
+++ project/FrontendCommands.scala
@@ -2,9 +2,9 @@
  * Frontend build commands.
  * Change these if you are using some other package manager. i.e: Yarn
  */
-object FrontendCommands {
-	val dependencyInstall: String = "npm install"
-	val test: String = "npm run test"
-	val serve: String = "npm run start"
-	val build: String = "npm run build"
-}
+//object FrontendCommands {
+//	val dependencyInstall: String = "npm install"
+//	val test: String = "npm run test"
+//	val serve: String = "npm run start"
+//	val build: String = "npm run build"
+//}
