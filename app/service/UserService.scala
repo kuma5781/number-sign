5d096d41f65fc77d6f9c045049d5cd6538bdecca
nnnaoki1998 <naoki101130@icloud.com>
2021-09-05 13:44:44 +0900

    UserServiceに処理を追加

--- app/service/UserService.scala
+++ app/service/UserService.scala
@@ -1,6 +1,6 @@
 package service
 
-import domain.`object`.user.{ User, UserId }
+import domain.`object`.user.{NewUser, User, UserId}
 import repository.UserRepository
 
 import scala.util.Try
@@ -9,4 +9,7 @@ class UserService(userRepository: UserRepository = new UserRepository) {
 
   def findAll(): Try[Seq[User]] = userRepository.findAll()
   def findBy(userId: UserId): Try[User] = userRepository.findBy(userId)
+  def save(newUser: NewUser): Try[Int] = userRepository.save(newUser)
+  def update(user: User): Try[Int] = userRepository.update(user)
+  def removeBy(userId: UserId): Try[Int] = userRepository.removeBy(userId)
 }
